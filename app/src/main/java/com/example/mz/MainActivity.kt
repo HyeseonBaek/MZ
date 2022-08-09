ea76a25bfeeb46ad5e56bd05d0c614fa15f68f6e
HyeseonBaek <bhs12087@naver.com>
2022-08-09 20:13:30 +0900

    confirm

--- app/src/main/java/com/example/mz/MainActivity.kt
+++ app/src/main/java/com/example/mz/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.mz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-// 여름 프로젝트 청년정책 활용정보 제공 어플리케이션
+// 여름 프로젝트 청년정책 활용정보 제공 어플리케이션 220809~
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
